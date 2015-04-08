6ae410bf99e32ffed3ea70582ea2fa78d5c33ae3
Ruben Albor Cerda <trancemeister75@gmail.com>
2015-04-08 00:16:04 -0500

    Commit Inicial
    - Player
    - Piso
    - Techo
    - Fisica de salto
    - Sistema de Particulas

---
+++ RocketMouse/Assets/Scripts/MouseController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+	public float jetpackforce = 75.0f;
+	Rigidbody2D body;
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		bool jetpackactive = Input.GetButton ("Fire1");
+		if(jetpackactive)
+		{
+			body.AddForce(new Vector2(0,jetpackforce));
+		}
+	}
+}
