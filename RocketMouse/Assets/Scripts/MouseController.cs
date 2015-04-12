00b9655e9e8cc535b4ff30fc38eb249224312d3f
Ruben Albor Cerda <trancemeister75@gmail.com>
2015-04-12 08:22:54 -0500

    BIG Update
    - Se agrega animacion al caminar
    - Switch entire animations (Run y Fly)
    - Se agregan multiples Sprites por Frame
    - Se agrega On/Off del jetpack

--- RocketMouse/Assets/Scripts/MouseController.cs
+++ RocketMouse/Assets/Scripts/MouseController.cs
@@ -4,10 +4,16 @@ using System.Collections;
 public class MouseController : MonoBehaviour {
 	public float jetpackforce = 75.0f;
 	public float forwardMovementSpeed = 3.0f;
+	public Transform groundCheckTransform;
+	private bool grounded;
+	public LayerMask groundCheckLayerMask;
+	Animator animator;
 	Rigidbody2D body;
+	public ParticleSystem jetpack;
 	// Use this for initialization
 	void Start () {
 		body = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -25,5 +31,19 @@ public class MouseController : MonoBehaviour {
 		Vector2 newVelocity = body.velocity;
 		newVelocity.x = forwardMovementSpeed;
 		body.velocity = newVelocity;
+		updateGroundedStatus ();
+		AdjustJetpack (jetpackactive);
+	}
+
+	void updateGroundedStatus()
+	{
+		grounded = Physics2D.OverlapCircle (groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+		animator.SetBool ("grounded", grounded);
+	}
+
+	void AdjustJetpack(bool jetpackActive)
+	{
+		jetpack.enableEmission = !grounded;
+		jetpack.emissionRate = jetpackActive ? 300.0f : 75.0f;
 	}
 }
